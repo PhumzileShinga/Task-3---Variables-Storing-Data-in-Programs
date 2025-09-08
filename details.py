6a531ecb07030592b141b0c75e9aba4a03e5afad
Phumzile Shinga <phumzile.shinga@yahoo.com>
2025-09-08 12:38:04 +0200

    Add files via upload

---
+++ details.py
@@ -0,0 +1,26 @@
+# Pseudocode: 
+"""
+# Ask for the user name
+# Ask for the user age
+# Ask for the user house number
+# Ask for the user street name
+# Print a sentence containing all the user details
+
+"""
+# Python code:
+
+# Ask for the user name
+user_name = input("Please enter your name: ")
+
+
+# Ask for the user age
+user_age = input("Please enter your age: ")
+
+# Ask for the user house number
+house_number = input("Please enter your house number: ")
+
+# Ask for the user street name
+street_name = input("Please enter your street name: ")
+
+# Print a sentence containing all the user details
+print(f"This is {user_name}.They are {user_age} years old and live at house number {house_number} on {street_name}.")
